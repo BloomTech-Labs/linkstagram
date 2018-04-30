d23aef9018fb041150893ccf7d3a8da78107de9e
FJF616 <franzjfernando@hotmail.com>
2018-04-30 15:39:13 -0700

    adds PicturePage and Sidebar Component

--- src/components/PicturePage.js
+++ src/components/PicturePage.js
@@ -1,17 +1,69 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Button } from 'react-strap';
+import 'bootstrap/dist/css/bootstrapmin.css';
+// import Sidebar from './SideBar';
 class PicturePage extends Component {
-    render(){
-      return(
-        <div >
-        Picture Page
-        </div>
-        );
+    constructor(props) {
+      super(props);
+      state = {
+        image,
+        link,
+        title
+      };
     }
-}
+    handleClick() {
+      //update input fields
+      this.setState({
+        image : image,
+        link : link,
+        title : title
+      });
+    }
+    render(){
+      const { image, link, title } = this.state;
+      return (
+              <Grid fluid className="grid-figure">
+              <nav class="navbar-text navbar-left">
+              <ol class="breadcrumb">
+                <li><a href="#Home">Home</a></li>
+                <li href="active">Picture Name</li>
+              </ol>
+              <p class="navbar-text navbar-right">
+              <a href="#SignOut" class="navbar-link">SignOut</a>
+              </p>
+              </nav>
+               <Row>
+                  <Col className='pictureName' xs={4} md={8} mdOffset={5} style={{ 'textAlign': 'center', 'width': '1145px'}}>
+                      <h1>{title}</h1>
+                  </Col>
+                </Row>
+                  <br/>
+                  <div >
+                    <Row >
+                      <Col className="Sidebar"xs={12} md={3}>
+                        <SideBar/> 
+                      </Col>
+                      <Col className="image" xs={12} md={3} >
+                        <img src={image} alt={image.title} />
+                      </Col>
+                      <Col className="affiliateLink" md={8} style={{ 'paddingTop': '20px'}}>
+                       <Link to={link}> </Link>
+                      </Col>
+                    </Row> 
+                    <Row className="submit">
+                      <Button outline color="primary" className="float-right" onClick={()=>this.handleClick()}>Submit</Button>
+                    </Row>
+                  </div>
+              </Grid>
+           )
+        }
+      };
 
+ 
 const mapStateToProps = state => {
   return {
     authenticated: state.auth.authenticated
@@ -19,3 +71,7 @@ const mapStateToProps = state => {
 };
 
 export default withRouter(connect(mapStateToProps)(PicturePage));
+
+
+
+
