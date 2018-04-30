7e0c95f14f0468a75bd1139834be54876eaf0dbb
Capril1042 <Caprilewis1042@gmail.com>
2018-04-30 18:54:15 -0400

    WIP- added SideBar ans SignUp component/ started working on Settings page

--- src/components/SettingsPage.js
+++ src/components/SettingsPage.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Header from './Header';
+import SideBar from './SideBar';
+
+// TODO 
+// 1. Replace email place holder with {user.email} ?
+// 2. handle Submit button
+
+
 class SettingsPage extends Component {
     render(){
-      return(
+      return (
         <div >
         <Header />
-        Settings
+        <SideBar/>
+        <form>
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" class="form-control" placeholder="Email"/> 
+          </div>
+          <div class="form-group">
+            <label for="old-password">Old password:</label>
+            <input type="password" class="form-control" placeholder="********"/> 
+          </div>
+          <div class="form-group">
+            <label for="new-password">New password:</label>
+            <input type="password" class="form-control" placeholder="********"/> 
+          </div>
+          <button type="submit" class="btn btn-primary">Update Password</button>
+        </form>
         </div>
         );
     }
