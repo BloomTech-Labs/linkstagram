5fed55344e11a71bc42f4986af77e6d36778fa2c
Michael Dennis <michaeladennis@yahoo.com>
2018-04-30 11:29:11 -0700

    adding basic shells of files.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { withRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import DefaultPage from './components/DefaultPage';
+import Notfound from './components/NotFound';
+import './index.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router >
+        <Switch>
+          <Route path="/" component={DefaultPage} exact />
+          <Route component={Notfound} />
+        </Switch>
+      </Router>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
