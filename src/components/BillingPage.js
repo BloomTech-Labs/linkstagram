45b00d80d4d23b83a0dc936058e2c7c7a368657d
Ismail Jadun <1683380+pinwale@users.noreply.github.com>
2018-05-01 13:16:29 -0400

    add nonfunctional billing form

--- src/components/BillingPage.js
+++ src/components/BillingPage.js
@@ -3,14 +3,37 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Header from './Header';
 class BillingPage extends Component {
-    render(){
-      return(
-        <div >
+  render(){
+    return(
+      <div >
+
         <Header />
-        Billing
+
+        <div>
+        <h2>Billing</h2>
+        <form className='payment-info' autocomplete='on' onSubmit=''>
+          <fieldset>
+            <label for='credit-card-number'>Credit Card Number</label>
+            <input id='credit-card-number' autoComplete='cc-number' type="text" />
+
+            <label for='expiration-date'>Expiration Date</label>
+            <input id='expiration-date' type='month' autoComplete='cc-exp' />
+
+            <label for='ccv-number'>CCV</label>
+            <input id='ccv-number' autoComplete='cc-csc' />
+          </fieldset>
+
+          <input id='subscription' type='checkbox' />
+          <label for='subscription'>
+            get me 1 month subscription for $20, brother.
+          </label>
+
+          <button name='submit'>Buy Now</button>
+        </form>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
