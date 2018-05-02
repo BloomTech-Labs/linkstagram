71f2698b217c420665a17e10fd35bce3550ac385
FJF616 <franzjfernando@hotmail.com>
2018-05-02 00:59:22 -0700

    merging changes

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { Parallax } from 'react-parallax';
-<<<<<<< HEAD
-//not quite sure what is wanted here from the parallax effect so I will 
-=======
 import { UncontrolledCarousel } from 'reactstrap';
 
 import Header from './Header';
 
->>>>>>> master
 class DefaultPage extends Component {
   render() {
     return (
