ef9f934203160144dedb27cf3a64e49558d55ae2
FJF616 <franzjfernando@hotmail.com>
2018-05-02 20:19:58 -0700

    changes instagram login helper to class component

--- src/components/InstagramSignIn/InstagramSignIn.js
+++ src/components/InstagramSignIn/InstagramSignIn.js
@@ -1,15 +1,16 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
 import InstagramLogin from 'react-instagram-login';
 // import FontAwesome from 'font-awesome';
 
+class InstaLogin extends Component {
 
-
-const responseInstagram = (response) => {
-  console.log(response);
-}
+  
  
-export default () => {
+render() {
+  const responseInstagram = (response) => {
+    console.log(response);
+  }
   return(
     <InstagramLogin
       clientId="5fd2f11482844c5eba963747a5f34556"
@@ -22,5 +23,7 @@ export default () => {
       /> */}
       <span> Sign In with Instagram </span>
     </InstagramLogin>
-  );
+    );
+  }
 }
+export default withRouter(InstaLogin);
