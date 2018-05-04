4ad78128bce42cc649468e88d130fbb3f3c63831
FJF616 <franzjfernando@hotmail.com>
2018-05-04 15:21:22 -0700

    adds SideBar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-
-// *** TODD***
-// Side Bar BOX
-// Appears on Dashboard
-// Picture Page
-// Billing Page and 
-// Setting Page
-
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import StickyBox from 'react-sticky-box'; 
 
 class SideBar extends Component {
-    render() {
-      return (
-        <div>
-            SideBar Box
+    render(){
+      return(
+        <div >
+            <StickyBox style={{ 'display': 'flex', 'alignItems': 'flex-start', 'padding': '30px 20px' }}>
+                <ul>
+                    <li><a href="#Pictures">Pictures</a></li>
+                    <li><a href="#Billing">Billing</a></li>
+                    <li><a href="#Settings">Settings</a></li>
+                </ul>
+            </StickyBox>
         </div>
-      );
+        );
     }
 }
 
-export default SideBar;
+const mapStateToProps = state => {
+  return {
+    authenticated: state.auth.authenticated
+  };
+};
 
+export default withRouter(connect(mapStateToProps)(Sidebar));
