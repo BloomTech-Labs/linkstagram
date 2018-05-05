84d296cdc2e56a821bd19830932539ba856e4c73
Ismail Jadun <1683380+pinwale@users.noreply.github.com>
2018-05-04 20:24:32 -0400

    Add Stripe checkout component & Stripe actions/reducer. AAlso ad express server.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import AuthReducer from './auth';
 import { reducer as FormReducer } from 'redux-form';
 import UserReducer from './user';
 import AlertsReducer from './alerts';
+import StripeReducer from './stripe';
 
 const rootReducer = combineReducers({
   alert: AlertsReducer,
   auth: AuthReducer,
   form: FormReducer,
-  users: UserReducer
+  users: UserReducer,
+  payment: StripeReducer
 });
 export default rootReducer;
