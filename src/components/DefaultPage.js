4a79a69bce2c8faab36655269d9515d03511f8ac
Capril1042 <Caprilewis1042@gmail.com>
2018-05-07 12:41:07 -0400

    Boiler plate for lading page Carousel

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { Parallax } from 'react-parallax';
-import { UncontrolledCarousel } from 'reactstrap';
+
 
 import Header from './Header';
+import Carousel from './Carousel';
 
 class DefaultPage extends Component {
   render() {
     return (
       <div> 
           <Header/>
-          
+          <Carousel>
           <h1> LINKSTAGRAM </h1>
           <Link to="/Billing">BUY NOW </Link>
         </div>
