8b8e4a356d83fe1146278ef770f26b034b025293
Ismail Jadun <1683380+pinwale@users.noreply.github.com>
2018-05-07 14:23:59 -0400

    change stripe_id to String type

--- Api/Schema/stripeUser.model.js
+++ Api/Schema/stripeUser.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Scheme = mongoose.Schema;
 const StripeUserSchema = Scheme({
     user_id : { type: Schema.Types.ObjectId, ref: 'LinkstasiteUser'},
-    stripe_id : {type:Number, require:true, unique:true},//not sure if this is correct TODO :: Ask Ismail
+    stripe_id : {type:String, require:true, unique:true}, //not sure if this is correct TODO :: Ask Ismail
     created : { type: Date, default: Date.now, required:true},
     last_updated : { type: Date, default: Date.now },
 });
