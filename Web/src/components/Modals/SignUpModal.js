74d70d2a24e63a6884da2df43003031ea9d0e4da
Ismail Jadun <1683380+pinwale@users.noreply.github.com>
2018-05-10 19:29:30 -0400

    added more saner lorem ipsum

--- Web/src/components/Modals/SignUpModal.js
+++ Web/src/components/Modals/SignUpModal.js
@@ -16,7 +16,7 @@ class SignUpModal extends Component {
         return (<Modal isOpen={this.props.modal} toggle={this.toggle.bind(this)} className="">
           <ModalHeader toggle={this.toggle.bind(this)}>Modal title</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            The is a Sign Up Modal.
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={this.toggle.bind(this)}>Do Something</Button>{' '}
